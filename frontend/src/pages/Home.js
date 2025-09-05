b6749fb573e9a4b8ec70a6520837bf84f8f0c671
LiuShiHao <152656883+cback1998@users.noreply.github.com>
2025-09-05 12:03:52 -0700

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h2>Welcome to Genome Finder</h2>
+      <p>Select a tool from the sidebar to get started.</p>
+    </div>
+  );
+};
+
+export default Home;
