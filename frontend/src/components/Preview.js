b9ca3a7873becee825c6c100849c5a3e1f2cc223
LiuShiHao <152656883+cback1998@users.noreply.github.com>
2025-09-05 12:15:01 -0700

    Create Preview.js

---
+++ frontend/src/components/Preview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Preview() {
+  return (
+    <div>
+      <h2>Preview</h2>
+      {/* This component will render preview of content */}
+    </div>
+  );
+}
+
+export default Preview;
