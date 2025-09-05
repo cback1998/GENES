2fbdf1c85959d9102e7547c28f41e6a1cff3461d
LiuShiHao <152656883+cback1998@users.noreply.github.com>
2025-09-05 11:36:27 -0700

    Create editorController.js

---
+++ backend/controllers/editorController.js
@@ -0,0 +1,9 @@
+// Editor controller
+class EditorController {
+  static save(req, res) {
+    // TODO: implement save logic
+    res.json({ message: 'Save successful' });
+  }
+}
+
+module.exports = EditorController;
